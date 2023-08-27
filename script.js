28e72d2e3b2fe499031c8488c950d39b382e4e30
Daniela <danielacardona2203@gmail.com>
2023-08-27 18:29:31 +0200

    Implement async function for fetching jokes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // https://api.chucknorris.io/jokes/random
 
-const joke = document.querySelector(".joke");
-const btn = document.querySelector(".btn");
+const joke = document.querySelector('.joke');
+const btn = document.querySelector('.btn');
 const url = 'https://api.chucknorris.io/jokes/random';
 
-btn.addEventListener("click", getJoke);
+btn.addEventListener('click', getJoke);
 
 // function getJoke() {
 //   fetch(url)
@@ -14,7 +14,16 @@ btn.addEventListener("click", getJoke);
 //     .then((data) => {
 //     console.log(data)
 //     joke.innerHTML = data.value;
- 
+
 //   })
 // }
 
+async function getJoke() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    joke.innerHTML = data.value;
+  } catch (error) {
+    joke.innerHTML = error.message;
+  }
+}
